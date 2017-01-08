6e313935ccaaac9d61f5322788ac95a71757f4c4
David Nimmo <dnimmo@gmail.com>
2017-01-08 13:39:33 +0000

    added architecture example

---
+++ Architecture.elm
@@ -0,0 +1,49 @@
+module Architecture exposing (..)
+
+import Html exposing (..)
+import Html.App exposing (beginnerProgram)
+import Html.Events exposing (..)
+
+
+type alias Model =
+    { showFace : Bool
+    }
+
+
+model =
+    { showFace = False }
+
+
+type Msg
+    = ShowFace
+
+
+update : Msg -> Model -> Model
+update msg model_ =
+    case msg of
+        ShowFace ->
+            if model_.showFace then
+                { model_ | showFace = False }
+            else
+                { model_ | showFace = True }
+
+
+view : Model -> Html Msg
+view model_ =
+    div []
+        [ h1 [] [ text "Face generator" ]
+        , button [ onClick ShowFace ] [ text "Face me" ]
+        , if model_.showFace then
+            text "=^__^="
+          else
+            text ""
+        ]
+
+
+main : Program Never
+main =
+    beginnerProgram
+        { model = model
+        , update = update
+        , view = view
+        }
